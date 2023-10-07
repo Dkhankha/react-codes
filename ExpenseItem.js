bb2e4562c1ccec2ef2688e46fbe6b03633520d40
Dkhankha <140619121+Dkhankha@users.noreply.github.com>
2023-10-07 22:26:04 +0530

    with delete button

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,17 +1,23 @@
+"use client"
 import React from 'react'
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import ExpenseDetails from './ExpenseDetails';
 import "./ExpenseItem.css"
+
 const ExpenseItem = (props) => {
+  const deleteHandler = () => {
+    document.getElementById(props.id).remove();
+  }
+  
     return (
-    <>
-    <div className='expense-item'>
-    <div>{props.date.toISOString()}</div>
-    <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <h2>{props.LocationOfExpenditure}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+    <div id={props.id}>
+    <Card className='expense-item'>
+    <ExpenseDate date={props.date}/>
+    <ExpenseDetails amount={props.amount} title={props.title} LocationOfExpenditure={props.LocationOfExpenditure}/>
+    <button className='btn' onClick={deleteHandler}>Delete</button>
+    </Card>
     </div>
-    </div>
-    </>
   )
 }
 
