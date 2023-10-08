52e48014fafa90be8e8f4f6dafae2b571e79d708
Dkhankha <140619121+Dkhankha@users.noreply.github.com>
2023-10-08 19:30:53 +0530

    on click amount changes

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,21 +1,23 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import ExpenseDetails from './ExpenseDetails';
 import "./ExpenseItem.css"
 
 const ExpenseItem = (props) => {
+  const [amount,setAmount] = useState(props.amount) 
   const deleteHandler = () => {
-    document.getElementById(props.id).remove();
+    setAmount("100")    
+    // document.getElementById(props.id).remove();
   }
   
     return (
     <div id={props.id}>
     <Card className='expense-item'>
     <ExpenseDate date={props.date}/>
-    <ExpenseDetails amount={props.amount} title={props.title} LocationOfExpenditure={props.LocationOfExpenditure}/>
-    <button className='btn' onClick={deleteHandler}>Delete</button>
+    <ExpenseDetails amount={amount} title={props.title} LocationOfExpenditure={props.LocationOfExpenditure}/>
+    <button className='btn' onClick={deleteHandler}>change</button>
     </Card>
     </div>
   )
