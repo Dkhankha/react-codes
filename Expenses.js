212f4be4a81053f073be4d4abb1ee55ff65dae0b
Dkhankha <140619121+Dkhankha@users.noreply.github.com>
2023-10-12 22:26:51 +0530

    work filter feature

--- Expenses.js
+++ Expenses.js
@@ -7,15 +7,20 @@ import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020")
-  
+
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear)
   }
- 
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {props.items.map((expense) => (
+      <ExpensesFilter selected={filteredYear} 
+      onChangeFilter={filterChangeHandler} 
+      />
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
